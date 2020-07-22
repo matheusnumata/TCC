954122c260c5057616f02e6cf094719c29803eed
matheusnumata <matheus.numata@gmail.com>
2020-07-22 10:38:01 -0300

    dgf

--- Site/public/_scripts/captureGET.js
+++
@@ -1,9 +0,0 @@
-function captureGET( name, url ) {
-  if (!url) url = location.href;
-  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-  var regexS = "[\\?&]"+name+"=([^&#]*)";
-  var regex = new RegExp( regexS );
-  var results = regex.exec( url );
-  return results == null ? null : results[1];
-}
-//gup('q', 'hxxp://example.com/?q=abc')

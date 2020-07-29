0ded7f3c3691a3718c6725e7ec0de593313e9d5e
matheusnumata <matheus.numata@gmail.com>
2020-07-29 20:15:11 -0300

    coleta de tweets funcionando

--- twitter.py
+++ twitter.py
@@ -1,6 +1,8 @@
 from __future__ import division
 from sklearn.model_selection import train_test_split
 from stop_words import get_stop_words
+from nltk.tokenize import TweetTokenizer
+from nltk.tokenize import sent_tokenize, word_tokenize
 import matplotlib.pyplot as plt
 from collections import Counter
 import pandas as pd
@@ -11,44 +13,51 @@ from tweepy import *
 import tweepy
 import csv
 import io
+from textblob import TextBlob
 
 auth = tweepy.OAuthHandler('Gges03ofUCtG7Fy4TI6Jy8XSE', 'woHaPvBJJSqYbbmjTnZnsy7vGIi6hq2qvIdY6t9BnVpWVPTO3u')
 auth.set_access_token('957936725275365376-SmyN2POrnlLlXvDUd0AvccUL1d2lwQH', 'Vp0xBunfHNRmyCyqDxZ5PV2lijnsmQJIUWTfcojM1BnSZ')
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-searchQuery = 'morte OR se mata OR desgraça OR morre OR felicidade OR te amo OR saudades'
+searchQuery = 'morte'
 
-public_tweets = api.search(q=searchQuery, count=100,
+public_tweets = api.search(q=searchQuery, count=10,
                             result_type="recent",
                             lang="pt-br")
 
-#public_tweets = api.user_timeline(screen_name='whindersson', count=100)
+with open('teste.csv', 'w', encoding='utf-8') as csvFile:
+            writer = csv.writer(csvFile, delimiter=',', dialect='excel', quotechar =  '"')
+            writer.writerow(["ID",
+                            "Usuário",
+                            "Tweet",
+                            "Seguidores",
+                            "Polarity",
+                            "Subjectivity",
+                            "Sentimento"])
 
-#abre/cria um arquivo csv
-#csvFile = open('teste.csv', 'w')
-
-with open('ofensivo.csv', 'w', encoding='utf-8') as csvFile:
-    writer = csv.writer(csvFile, delimiter=',', dialect='excel', quotechar =  '"')
-    writer.writerow(["ID",
-                    "Usuário",
-                    "Tweet",
-                    "Seguidores",
-                    "Sentimento"])
-
-    for tweet in public_tweets:
-        if tweet.text.startswith("RT @") == True:
-            print('This tweet is a retweet')
-        else:
-            print('This tweet is not retweet')
-            post = tweet.text
-            post = post.replace('\n', ' ')
-            writer.writerow(([str(tweet.user.id),
-                            str(tweet.user.screen_name),
-                            str(post),
-                            str(tweet.user.followers_count)]))
-        #print (tweet.text);
-def on_data(self, data):
-    datajson = json.loads(data)
-    if any([i for i in filterKeywords if i in datajson["text"]]):
-        """Do Desired function"""
+for tweet in public_tweets:
+    frase = TextBlob(tweet.text)
+    if tweet.text.startswith("RT @") == True:
+            print('oi')
+    elif frase.detect_language() != 'en':
+        traducao = TextBlob(str(frase.translate(to='en')))
+        print('Tweet: {0} - Sentimento: {1}'.format(tweet.text, traducao.sentiment))
+        post = tweet.text
+        post = post.replace('\n', ' ')
+        writer.writerow(([str(tweet.user.id),
+                        str(tweet.user.screen_name),
+                        str(post),
+                        str(tweet.user.followers_count),
+                        str(traducao.sentiment.polarity),
+                        str(traducao.sentiment.subjectivity)]))
+    else:
+        print('Tweet: {0} - Sentimento: {1}'.format(tweet.text, frase.sentiment))
+        post = tweet.text
+        post = post.replace('\n', ' ')
+        writer.writerow(([str(tweet.user.id),
+                        str(tweet.user.screen_name),
+                        str(post),
+                        str(tweet.user.followers_count),
+                        str(traducao.sentiment.polarity),
+                        str(traducao.sentiment.subjectivity)]))
